d89be9b3ac0f475e6fe4abe3f6b04e995d94c3ef
HotPotatoC <juandotulung@gmail.com>
2022-12-01 16:15:38 +0700

    perf: use `priority_queue`

--- day-1/main.cpp
+++ day-1/main.cpp
@@ -1,54 +1,47 @@
 #include <bits/stdc++.h>
 
-auto part_one() {
+auto parse_input() {
   std::ifstream input;
+  std::priority_queue<int> pq_calories;
 
   input.open("input.in");
 
-  std::vector<int> total_calories;
+  auto current = static_cast<int>(0);
 
-  int current = 0;
   std::string line;
   while (std::getline(input, line)) {
-    if (line == "") {
-      total_calories.push_back(current);
+    if (line.empty()) {
+      pq_calories.push(current);
       current = 0;
     } else {
       current += std::stoi(line);
     }
   }
 
-  auto highest_calorie =
-      *std::max_element(total_calories.begin(), total_calories.end());
+  return pq_calories;
+}
 
-  return highest_calorie;
+auto part_one() {
+  std::priority_queue<int> pq_calories = parse_input();
+
+  return pq_calories.top();
 }
 
 auto part_two() {
-  std::ifstream input;
+  std::priority_queue<int> pq_calories = parse_input();
 
-  input.open("input.in");
-
-  std::vector<int> total_calories;
-
-  int current = 0;
-  std::string line;
-  while (std::getline(input, line)) {
-    if (line == "") {
-      total_calories.push_back(current);
-      current = 0;
-    } else {
-      current += std::stoi(line);
-    }
+  auto ans = static_cast<int>(0);
+  auto i = static_cast<int>(0);
+  while (!pq_calories.empty() && i < 3) {
+    ans += pq_calories.top();
+    pq_calories.pop();
+    i++;
   }
 
-  std::partial_sort(total_calories.begin(), total_calories.begin() + 3,
-                    total_calories.end(), std::greater<int>());
-
-  return std::accumulate(total_calories.begin(), total_calories.begin() + 3, 0);
+  return ans;
 }
 
-auto main() -> int {
+int main() {
   std::cout << "Part one: " << part_one() << std::endl;
   std::cout << "Part two: " << part_two() << std::endl;
 
