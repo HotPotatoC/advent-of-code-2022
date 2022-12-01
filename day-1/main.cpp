523c16b1d5ccc4a4cd013914a5db1a8082503f37
HotPotatoC <juandotulung@gmail.com>
2022-12-01 15:58:16 +0700

    chore: initial commit 🚀

---
+++ day-1/main.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+auto part_one() {
+  std::ifstream input;
+
+  input.open("input.in");
+
+  std::vector<int> total_calories;
+
+  int current = 0;
+  std::string line;
+  while (std::getline(input, line)) {
+    if (line == "") {
+      total_calories.push_back(current);
+      current = 0;
+    } else {
+      current += std::stoi(line);
+    }
+  }
+
+  auto highest_calory =
+      *std::max_element(total_calories.begin(), total_calories.end());
+
+  return highest_calory;
+}
+
+auto part_two() {
+  std::ifstream input;
+
+  input.open("input.in");
+
+  std::vector<int> total_calories;
+
+  int current = 0;
+  std::string line;
+  while (std::getline(input, line)) {
+    if (line == "") {
+      total_calories.push_back(current);
+      current = 0;
+    } else {
+      current += std::stoi(line);
+    }
+  }
+
+  std::partial_sort(total_calories.begin(), total_calories.begin() + 3,
+                    total_calories.end(), std::greater<int>());
+
+  return std::accumulate(total_calories.begin(), total_calories.begin() + 3, 0);
+}
+
+auto main() -> int {
+  std::cout << "Part one: " << part_one() << std::endl;
+  std::cout << "Part two: " << part_two() << std::endl;
+
+  return 0;
+}
