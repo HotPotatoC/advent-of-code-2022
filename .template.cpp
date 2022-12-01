6ffc3cbcadef8d7a9cd8a8df0ef98d16fd4d08a1
HotPotatoC <juandotulung@gmail.com>
2022-12-01 16:47:12 +0700

    feat: added code gen

---
+++ .template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+auto parse_input() {
+  std::ifstream input;
+
+  input.open("input.in");
+
+  // parse input here
+
+  return 0;
+}
+
+auto part_one() {
+  auto input = parse_input();
+  //
+  return 0;
+}
+
+auto part_two() {
+  auto input = parse_input();
+  //
+  return 0;
+}
+
+int main() {
+  std::cout << "Part one: " << part_one() << std::endl;
+  std::cout << "Part two: " << part_two() << std::endl;
+
+  return 0;
+}
---
+++ new_day.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+PREFIX = "day-"
+
+
+def main():
+    """
+    Sequentially creates a new day_xx directory containing
+    the C++ starter code and input files
+    """
+    dirs = os.listdir()
+    days = sorted(filter(lambda s: s.startswith(PREFIX), dirs))
+
+    next_day = int(days[len(days) - 1][4:]) + 1
+    next_day_dir = PREFIX
+
+    if next_day < 10:
+        next_day_dir += "0"
+
+    next_day_dir += str(next_day)
+
+    if next_day not in days:
+        os.mkdir(next_day_dir)
+        shutil.copyfile("./.template.cpp", os.path.join(next_day_dir, "main.cpp"))
+        fp = open(os.path.join(next_day_dir, "input.in"), "w")
+        fp.close()
+
+
+if __name__ == "__main__":
+    main()
